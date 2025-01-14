0517161681b017e3568cfa7300185e79b78a2dbe
Kayshen-X <kayshen_xu@163.com>
2025-01-14 10:53:36 +0800

    feat:change db mysql

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,16 +4,14 @@ import { ConfigService } from '@nestjs/config';
 export const getDatabaseConfig = (
   configService: ConfigService,
 ): TypeOrmModuleOptions => ({
-  type: 'postgres',
+  type: 'mysql',
   host: configService.get('DB_HOST', 'localhost'),
-  port: configService.get('DB_PORT', 5432),
-  username: configService.get('DB_USERNAME', 'postgres'),
-  password: configService.get('DB_PASSWORD', 'postgres'),
+  port: configService.get('DB_PORT', 3306),
+  username: configService.get('DB_USERNAME', 'root'),
+  password: configService.get('DB_PASSWORD', 'root'),
   database: configService.get('DB_DATABASE', 'chat_diagram'),
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: configService.get('NODE_ENV', 'development') === 'development', // 仅在开发环境启用
-  extra: {
-    // 设置会话级别的时区
-    timezone: 'Asia/Shanghai',
-  },
+  synchronize: configService.get('NODE_ENV', 'development') === 'development',
+  charset: 'utf8mb4',
+  timezone: '+08:00',
 });
